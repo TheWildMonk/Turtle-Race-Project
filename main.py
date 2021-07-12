f4c0f12076efbf18a3bccc57e204faa6a268d915
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-12 22:47:26 +0600

    create initial file for the project

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+# Create necessary objects
+tim = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+
+# Screen exit initiator
+screen.exitonclick()
